6cf2819bdf6f192ec1982675295f824711a68c58
fireflicker <90106441+fireflicker@users.noreply.github.com>
2026-06-18 21:00:39 +0100

    Add focusable modifier to PlayerScreen

--- app/src/main/java/PlayerScreen.kt
+++ app/src/main/java/PlayerScreen.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
