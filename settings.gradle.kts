40d213ff8913592139e4affe69ce87eb3badc5ea
fireflicker <90106441+fireflicker@users.noreply.github.com>
2026-06-19 23:02:45 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,25 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        gopluginManagement {
+    repositories {
         google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FirePlex3.0"
+include(":app")
+ogle()
         mavenCentral()
     }
 }
