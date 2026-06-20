59ac0b72c52ad59a6da0b359b68a65ff819fa300
fireflicker <90106441+fireflicker@users.noreply.github.com>
2026-06-20 15:20:58 +0100

    Update PlexLibrary.kt

--- app/src/main/java/data/PlexLibrary.kt
+++ app/src/main/java/data/PlexLibrary.kt
@@ -1,11 +1,7 @@
 package com.fireflicker.fireplex2.data
 
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ExoPlayerSettings(
-    val preBufferSeconds: Int = 20,
-    val zoomMode: String = "fill",
-    val subtitlesEnabled: Boolean = false,
-    val volumePercent: Int = 100
+data class PlexLibrary(
+    val key: String,
+    val title: String,
+    val type: String
 )
